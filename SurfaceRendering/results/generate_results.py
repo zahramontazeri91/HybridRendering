969edbb94aae6ed99dacb73943a55ef209561620
zahramontazeri91 <zahramontazeri91@gmail.com>
2017-05-16 19:08:22 -0700

    results for tables added

---
+++ SurfaceRendering/results/generate_results.py
@@ -0,0 +1,44 @@
+from shutil import copyfile
+import subprocess
+import os
+
+os.chdir("C:/Users/Zahra/Documents/GitHub/SurfaceRendering/mitsuba/dist");
+
+#subprocess.call('mitsuba C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold170/untiled_hybrid.xml');
+#copyfile("C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold170/untiled_hybrid.exr", "C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/results/hybrid_vs_ref/170_hybrid_angle_45.exr")
+
+#subprocess.call('mitsuba C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold175/untiled_hybrid.xml');
+#copyfile("C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold175/untiled_hybrid.exr", "C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/results/hybrid_vs_ref/175_hybrid_angle_45.exr")
+#
+#subprocess.call('mitsuba C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold180/untiled_hybrid.xml');
+#copyfile("C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold180/untiled_hybrid.exr", "C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/results/hybrid_vs_ref/180_hybrid_angle_45.exr")
+
+#subprocess.call('mitsuba C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold185/untiled_hybrid.xml');
+#copyfile("C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold185/untiled_hybrid.exr", "C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/results/hybrid_vs_ref/185_hybrid_angle_45.exr")
+#
+#subprocess.call('mitsuba C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold190/untiled_hybrid.xml');
+#copyfile("C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold190/untiled_hybrid.exr", "C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/results/hybrid_vs_ref/190_hybrid_angle_45.exr")
+#
+subprocess.call('mitsuba C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold232/untiled_hybrid.xml');
+copyfile("C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold232/untiled_hybrid.exr", "C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/results/hybrid_vs_ref/232_hybrid_angle_45.exr")
+
+
+##### reference volume 
+#subprocess.call('mitsuba C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold170/untiled_hybrid.xml');
+#copyfile("C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold170/untiled_hybrid.exr", "C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/results/hybrid_vs_ref/reference_volume.exr")
+
+#### cropped volume 
+#subprocess.call('mitsuba C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold170/untiled_hybrid.xml');
+#copyfile("C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold170/untiled_hybrid.exr", "C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/results/height_vs_cropVol/170_cropped_vol.exr")
+
+#subprocess.call('mitsuba C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold175/untiled_hybrid.xml');
+#copyfile("C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold175/untiled_hybrid.exr", "C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/results/height_vs_cropVol/175_cropped_vol.exr")
+#
+#subprocess.call('mitsuba C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold180/untiled_hybrid.xml');
+#copyfile("C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold180/untiled_hybrid.exr", "C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/results/height_vs_cropVol/180_cropped_vol.exr")
+
+#subprocess.call('mitsuba C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold185/untiled_hybrid.xml');
+#copyfile("C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold185/untiled_hybrid.exr", "C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/results/height_vs_cropVol/185_cropped_vol.exr")
+#
+#subprocess.call('mitsuba C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold190/untiled_hybrid.xml');
+#copyfile("C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/Output/threshold190/untiled_hybrid.exr", "C:/Users/Zahra/Documents/GitHub/SurfaceRendering/SurfaceRendering/results/height_vs_cropVol/190_cropped_vol.exr")
